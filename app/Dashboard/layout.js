edaeaecc793f69819b3fe8cafe1fbe7b41919a03
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-04-21 10:04:37 +0600

    sign out task done successfully

--- app/Dashboard/layout.js
+++ app/Dashboard/layout.js
@@ -4,13 +4,13 @@ import Sidebar from "../components/Sidebar/Sidebar";
 const layout = ({ children }) => {
   return (
     <>
-      <Navbar />
-      <div className="flex">
-        <div>
-          <Sidebar />
+        <Navbar />
+        <div className="flex">
+          <div>
+            <Sidebar />
+          </div>
+          <div className="flex justify-center items-center">{children}</div>
         </div>
-        <div className="flex justify-center items-center">{children}</div>
-      </div>
     </>
   );
 };
--- app/components/Navbar/Navbar.js
+++ app/components/Navbar/Navbar.js
@@ -1,19 +1,42 @@
+'use client'
 import Image from "next/image";
 import logo from "@/public/images/logo.png";
 import Link from "next/link";
+import { useContext } from "react";
+import { AuthContext } from "@/app/context/AuthProvider";
 const Navbar = () => {
+  
+  const { userLogOut } = useContext(AuthContext);
+
+  const handleLogOut = () => {
+    userLogOut()
+      .then(() => {
+        // Sign-out successful.
+        console.log("logged out successfully");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <div className="navbar px-12 bg-[#3d2d2d] text-[#f1d1a8]">
         <div className="flex-1">
           <div className="flex items-center">
             <Image className="h-12 w-10" src={logo} alt="coffee cup" />
-            <p className="font-bold fon text-2xl tracking-wide uppercase">CaffeineCozy</p>
+            <p className="font-bold fon text-2xl tracking-wide uppercase">
+              CaffeineCozy
+            </p>
           </div>
         </div>
         <div className="flex-none">
-          <Link href='/'>
-            <button className="bg-[#f1d1a8] text-[#3d2d2d] uppercase font-bold px-6 py-2 rounded">
+          <Link href="/">
+            <button
+              onClick={handleLogOut}
+              className="bg-[#f1d1a8] text-[#3d2d2d] uppercase font-bold px-6 py-2 rounded"
+            >
               Sign Out
             </button>
           </Link>
