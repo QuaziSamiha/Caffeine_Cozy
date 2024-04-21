6166b3cf12ab4539dc0ff22b15dd4a780292face
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-04-22 00:32:11 +0600

    users page done

--- app/Dashboard/Users/page.js
+++ app/Dashboard/Users/page.js
@@ -4,36 +4,25 @@ import userImage from "@/public/images/user.jpg";
 import { useContext } from "react";
 import { AuthContext } from "@/app/context/AuthProvider";
 const Users = () => {
+  // users array is getting from AuthProvider component
   const { users } = useContext(AuthContext);
-  console.log(users);
+  // console.log(users);
   return (
     <>
-      <section className="mx-12 py-12">
-        <h1 className="text-center font-bold mb-12 text-3xl text-[#3d2d2d] uppercase">
+      <section className="mx-4 lg:mx-12 py-12">
+        <h1 className="text-center font-bold mb-12 text-lg sm:text-xl lg:text-3xl text-[#3d2d2d] uppercase">
           All Users
         </h1>
-        <section className="grid grid-cols-2 gap-10">
-          <div className="mx-4 bg-base-100 shadow-xl rounded p-6 flex justify-around items-center">
-            <Image className="h-24 w-24 rounded-full" src={userImage} alt="user" />
-            <div className="ml-4">
-              <p className="text-[#3d2d2d] font-semibold">Samiha Tasnim</p>
-              <p className="text-[#0f172a] text-sm font-medium">
-                quazisamiha@gmail.com
-              </p>
-              <button className="mt-3 bg-[#fce1c3] text-[#3d2d2d] px-3 py-1 rounded border-b-2 border-[#3d2d2d] text-sm font-semibold">
-                Make Admin
-              </button>
-            </div>
-          </div>
+        <section className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10">
           {users.map((user, index) => (
             <div key={index}>
-              <div className="mx-4 bg-base-100 shadow-xl rounded p-6 flex justify-around items-center">
+              <div className="mx-4 bg-base-100 shadow-xl rounded p-6 flex flex-col md:flex-row justify-around items-center">
                 <Image
                   className="h-24 w-24 rounded-full"
                   src={userImage}
                   alt="user"
                 />
-                <div className="ml-4">
+                <div className="mt-4 md:ml-4">
                   <p className="text-[#3d2d2d] font-semibold">{user.name}</p>
                   <p className="text-[#0f172a] text-sm font-medium">
                     {user.email}
